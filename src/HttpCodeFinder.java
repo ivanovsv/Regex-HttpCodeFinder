b7f1bc4e8ac01548a293bd11890c4425a23a2702
Vladimirs.Ivanovs <Vladimirs.Ivanovs@monitise.com>
2015-07-02 15:46:13 +0100

    Initial commit

---
+++ src/HttpCodeFinder.java
@@ -0,0 +1,57 @@
+/**
+ * Created by IvanovsV on 02/07/2015.
+ */
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
+import java.io.*;
+
+public class HttpCodeFinder {
+    private Map<String, Integer> httpCodes = new HashMap<String, Integer>();
+    private Pattern pattern = Pattern.compile("status=\"\\d{3}\"");
+
+    public void scanSingleFile(File file) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+
+        String line; // line of document to be scanned
+        String found;
+        while ((line = reader.readLine()) != null) {
+            Matcher matcher = pattern.matcher(line);
+            while (matcher.find()) {
+                found = matcher.group();
+                int count = httpCodes.containsKey(found) ? httpCodes.get(found)
+                        : 0;
+                httpCodes.put(found, count + 1);
+            }
+        }
+        reader.close();
+    }
+
+    public void printMap() {
+        for (String key : httpCodes.keySet()) {
+            System.out.println(key + " " + httpCodes.get(key));
+        }
+    }
+
+    public void scanAllFiles(File[] files) throws IOException {
+        for (File f : files) {
+            scanSingleFile(f);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        File dir = new File(".");
+        File[] files = dir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().endsWith(".xlg");
+            }
+        });
+
+        HttpCodeFinder finder = new HttpCodeFinder();
+
+        finder.scanAllFiles(files);
+        finder.printMap();
+    }
+}
